060839417d88d80fc3883e9443d5542ffb3bfb4a
Tomson0119 <kjs980119@gmail.com>
2021-03-24 18:07:52 +0900

    연습문제 12장 2번 준비

---
+++ Chapter 12/Ex 12-2/ex 12-2.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include "../../Common/myd3dApp.h"
+#include "../../Common/myFrameResource.h"
+#include "../../Common/myGeometryGenerator.h"
+
+#include "RenderItem.h"
+
+#include <DirectXColors.h>
+#include <array>
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+
+const int gNumFrameResources = 3;
+
+class MyUnitSphereApp : public MyD3DApp
+{
+public:
+
+	MyUnitSphereApp();
+	MyUnitSphereApp(const MyUnitSphereApp& rhs) = delete;
+	MyUnitSphereApp& operator=(const MyUnitSphereApp& rhs) = delete;
+	~MyUnitSphereApp();
+
+	virtual bool Initialize() override;
+
+private:
+
+	virtual void OnResize() override;
+	virtual void Update(const MyGameTimer& gt) override;
+	virtual void Draw(const MyGameTimer& gt) override;
+
+	virtual void OnMouseDown(WPARAM btnState, int x, int y) override;
+	virtual void OnMouseUp(WPARAM btnState, int x, int y) override;
+	virtual void OnMouseMove(WPARAM btnState, int x, int y) override;
+
+	void OnKeyboardInput(const MyGameTimer& gt);
+	void UpdateCamera(const MyGameTimer& gt);
+	void AnimateMaterials(const MyGameTimer& gt);
+	void UpdateObjectCBs(const MyGameTimer& gt);
+	void UpdateMaterialCBs(const MyGameTimer& gt);
+	void UpdateMainPassCB(const MyGameTimer& gt);
+
+	void LoadTextures();
+	void BuildRootSignature();
+	void BuildDescriptorHeaps();
+	void BuildShadersAndInputLayout();
+	void BuildSphereGeometry();
+	void BuildPSOs();
+	void BuildFrameResources();
+	void BuildMaterials();
+	void BuildRenderItems();
+
+	void DrawRenderItems(ID3D12GraphicsCommandList* cmdList,
+		const std::vector<RenderItem*>& ritems);
+
+	std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> GetStaticSamplers();
+
+private:
+
+	std::vector<std::unique_ptr<MyFrameResource>> mFrameResources;
+	MyFrameResource* mCurrFrameResource = nullptr;
+	int mCurrFrameResourceIndex = 0;
+
+	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
+
+	ComPtr<ID3D12DescriptorHeap> mSrvDescriptorHeap = nullptr;
+
+	std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> mGeometries;
+	std::unordered_map<std::string, std::unique_ptr<Material>> mMaterials;
+	std::unordered_map<std::string, std::unique_ptr<Texture>> mTextures;
+	std::unordered_map<std::string, ComPtr<ID3DBlob>> mShaders;
+	std::unordered_map<std::string, ComPtr<ID3D12PipelineState>> mPSOs;
+
+	std::vector<D3D12_INPUT_ELEMENT_DESC> mInputLayout;
+
+	// List of all the render items.
+	std::vector<std::unique_ptr<RenderItem>> mAllRitems;
+
+	// Render items divided by PSO.
+	std::vector<RenderItem*> mRitemLayer[(int)RenderLayer::Count];
+
+	PassConstants mMainPassCB;
+
+	XMFLOAT3 mEyePos = { 0.0f,0.0f,0.0f };
+	XMFLOAT4X4 mView = MyMathHelper::Identity4x4();
+	XMFLOAT4X4 mProj = MyMathHelper::Identity4x4();
+
+	float mTheta = 1.5f * XM_PI;
+	float mPhi = XM_PIDIV2 - 0.4f;
+	float mRadius = 40.0f;
+
+	float mSunTheta = 1.25f * XM_PI;
+	float mSunPhi = XM_PIDIV4;
+
+	POINT mLastMousePos = { 0, 0 };
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) || defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		MyUnitSphereApp app;
+		if (!app.Initialize())
+			return 0;
+
+		return app.Run();
+	}
+	catch (MyDxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
