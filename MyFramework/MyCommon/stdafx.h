4c1652667d6ecdc762ccbef8d50949e53e76054c
Tomson0119 <kjs980119@gmail.com>
2021-05-19 19:26:39 +0900

    Matrix, Vector, Math 인라인 함수 구축

--- MyFramework/MyCommon/stdafx.h
+++ MyFramework/MyCommon/stdafx.h
@@ -35,7 +35,10 @@
 
 // C++ 헤더 파일:
 #include <vector>
+#include <stack>
+#include <unordered_map>
 #include <string>
+#include <memory>
 
 
 using namespace DirectX;
@@ -59,3 +62,69 @@ inline std::wstring AnsiToWString(const std::string& str)
 	if (FAILED(hr))	{ throw DxException(hr, L#f, wfn, __LINE__); } \
 }																		
 #endif
+
+
+////////////////////////////////////////////////////////////////////////////
+
+namespace Math
+{
+	const float PI = 3.1415926535f;
+}
+
+namespace Vector3
+{
+	inline XMFLOAT3 VectorToFloat3(FXMVECTOR& vector)
+	{
+		XMFLOAT3 ret;
+		XMStoreFloat3(&ret, vector);
+		return ret;
+	}
+
+	inline XMFLOAT3 Replicate(float value)
+	{
+		return VectorToFloat3(XMVectorReplicate(value));
+	}
+
+	inline XMFLOAT3 MultiplyAdd(float delta, XMFLOAT3& src, XMFLOAT3& dst)
+	{		
+		XMVECTOR v1 = XMLoadFloat3(&Replicate(delta));
+		XMVECTOR v2 = XMLoadFloat3(&src);
+		XMVECTOR v3 = XMLoadFloat3(&dst);
+		return VectorToFloat3(XMVectorMultiplyAdd(v1, v2, v3));
+	}
+
+	inline XMFLOAT3 TransformNormal(XMFLOAT3& src, FXMMATRIX& mat)
+	{
+		return VectorToFloat3(XMVector3TransformNormal(XMLoadFloat3(&src), mat));
+	}
+
+	inline XMFLOAT3 Normalize(XMFLOAT3& v)
+	{
+		return VectorToFloat3(XMVector3Normalize(XMLoadFloat3(&v)));
+	}
+
+	inline XMFLOAT3 Subtract(XMFLOAT3& v1, XMFLOAT3& v2)
+	{
+		return VectorToFloat3(XMVectorSubtract(XMLoadFloat3(&v1), XMLoadFloat3(&v2)));
+	}
+
+	inline XMFLOAT3 Cross(XMFLOAT3& v1, XMFLOAT3& v2)
+	{
+		return VectorToFloat3(XMVector3Cross(XMLoadFloat3(&v1), XMLoadFloat3(&v2)));
+	}
+
+	inline float Dot(XMFLOAT3& v1, XMFLOAT3& v2)
+	{
+		return XMVectorGetX(XMVector3Dot(XMLoadFloat3(&v1), XMLoadFloat3(&v2)));
+	}
+}
+
+namespace Matrix4x4
+{
+	inline XMFLOAT4X4 Identity4x4()
+	{	
+		XMFLOAT4X4 ret;
+		XMStoreFloat4x4(&ret, XMMatrixIdentity());
+		return ret;
+	}
+}
