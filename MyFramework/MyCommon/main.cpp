6c418c19a4956d17b2f947bec715440461718fcc
Tomson0119 <kjs980119@gmail.com>
2021-05-19 11:43:01 +0900

    일반 파일 제거

--- MyFramework/MyCommon/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "d3dFramework.h"
-
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, int ShowCmd)
-{
-	D3DFramework d3dApp;
-	d3dApp.InitFramework();
-	d3dApp.Run();
-}
