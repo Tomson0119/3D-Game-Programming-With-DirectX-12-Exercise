a19bf502ca2a2e0df2b5ea3d5835d1e3603bc55e
Tomson0119 <kjs980119@gmail.com>
2021-05-28 23:16:29 +0900

    카메라 Scene에서 관리, 상수 버퍼 뷰 생성 및 루트 매개변수 추가

--- MyFramework/MyProject/gameScene.cpp
+++ MyFramework/MyProject/gameScene.cpp
@@ -3,6 +3,7 @@
 
 GameScene::GameScene()
 {
+	mCamera = std::make_unique<Camera>(0.25f * Math::PI, 1.0f, 1.0f, 1000.0f);
 }
 
 GameScene::~GameScene()
@@ -12,15 +13,22 @@ GameScene::~GameScene()
 void GameScene::BuildObjects(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList)
 {
 	BuildRootSignature(device);
+	BuildShaders();
+	BuildConstantBuffers(device);
 	BuildPSOs(device);
+}
 
+void GameScene::Update(const GameTimer& timer)
+{
+	mCamera->UpdateViewMatrix();
 }
 
 void GameScene::Draw(ID3D12GraphicsCommandList* cmdList, const GameTimer& timer)
 {
 	cmdList->SetGraphicsRootSignature(mRootSignature.Get());
 
-	// cmdList->SetGraphicsRootConstantBufferView(0, virtualAdress)
+	auto common = mCommonCB->Resource();	
+	cmdList->SetGraphicsRootConstantBufferView(0, common->GetGPUVirtualAddress());
 	
 	for (const auto& pso : mPipelines)
 	{
@@ -29,14 +37,19 @@ void GameScene::Draw(ID3D12GraphicsCommandList* cmdList, const GameTimer& timer)
 	}
 }
 
+void GameScene::OnProcessMouseInput(UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+}
+
 void GameScene::OnProcessKeyInput(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 }
 
 void GameScene::BuildRootSignature(ID3D12Device* device)
 {
-	CD3DX12_ROOT_PARAMETER parameters[1];	
-	parameters[0].InitAsConstantBufferView(0);
+	CD3DX12_ROOT_PARAMETER parameters[2];	
+	parameters[0].InitAsConstantBufferView(0);  // CommonCB
+	parameters[1].InitAsConstantBufferView(1);  // ObjectCB
 
 	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(1, parameters, 0, nullptr,
 		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
@@ -54,10 +67,19 @@ void GameScene::BuildRootSignature(ID3D12Device* device)
 		IID_PPV_ARGS(&mRootSignature)));
 }
 
+void GameScene::BuildGameObjects()
+{
+}
+
 void GameScene::BuildShaders()
 {
 	auto colorShader = std::make_unique<ColorShader>();
 	mShaders["color"] = std::move(colorShader);
+}
+
+void GameScene::BuildConstantBuffers(ID3D12Device* device)
+{
+	mCommonCB = std::make_unique<ConstantBuffer<CommonConstants>>(device, 1);
 }
 
 void GameScene::BuildPSOs(ID3D12Device* device)
--- MyFramework/MyProject/gameScene.h
+++ MyFramework/MyProject/gameScene.h
@@ -1,8 +1,18 @@
 #pragma once
 
 #include "../MyCommon/gameTimer.h"
+#include "../MyCommon/camera.h"
+#include "../MyCommon/constantBuffer.h"
+
 #include "pipeline.h"
 #include "shader.h"
+
+
+struct CommonConstants
+{
+	XMFLOAT4X4 View;
+	XMFLOAT4X4 Proj;
+};
 
 class GameScene
 {
@@ -13,21 +23,30 @@ public:
 	virtual ~GameScene();
 
 	void BuildObjects(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList);
+
+	void Update(const GameTimer& timer);
 	void Draw(ID3D12GraphicsCommandList* cmdList, const GameTimer& timer);
 
+	void OnProcessMouseInput(UINT uMsg, WPARAM wParam, LPARAM lParam);
 	void OnProcessKeyInput(UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 	XMFLOAT4 GetFrameColor() const { return mFrameColor; }
 
 private:
 	void BuildRootSignature(ID3D12Device* device);
+	void BuildGameObjects();
 	void BuildShaders();
+	void BuildConstantBuffers(ID3D12Device* device);
 	void BuildPSOs(ID3D12Device* device);
 
 private:
 	XMFLOAT4 mFrameColor = (XMFLOAT4)Colors::Black;
 
-	ComPtr<ID3D12RootSignature> mRootSignature;
+	std::unique_ptr<Camera> mCamera;
+	std::unique_ptr<ConstantBuffer<CommonConstants>> mCommonCB;
+
+	ComPtr<ID3D12RootSignature> mRootSignature;	
+	
 	std::unordered_map<std::string, std::unique_ptr<Shader>> mShaders;
 	std::unordered_map<std::string, std::unique_ptr<Pipeline>> mPipelines;
 };
