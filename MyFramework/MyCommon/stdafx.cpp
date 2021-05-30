adb1eadd00e1ff93a145a3ccc748da60ea5cb30c
Tomson0119 <kjs980119@gmail.com>
2021-05-30 16:09:58 +0900

    리소스 버퍼 생성 함수 일부 수정

--- MyFramework/MyCommon/stdafx.cpp
+++ MyFramework/MyCommon/stdafx.cpp
@@ -12,21 +12,25 @@ ComPtr<ID3D12Resource> CreateBufferResource(
         &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
         D3D12_HEAP_FLAG_NONE,
         &CD3DX12_RESOURCE_DESC::Buffer(byteSize),
-        D3D12_RESOURCE_STATE_COPY_DEST, NULL,
-        IID_PPV_ARGS(&defaultResource)));
+		D3D12_RESOURCE_STATE_COPY_DEST, 
+		nullptr, IID_PPV_ARGS(defaultResource.GetAddressOf())));
 
 	ThrowIfFailed(device->CreateCommittedResource(
 		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
 		D3D12_HEAP_FLAG_NONE,
 		&CD3DX12_RESOURCE_DESC::Buffer(byteSize),
 		D3D12_RESOURCE_STATE_GENERIC_READ,
-		NULL, IID_PPV_ARGS(&uploadBuffer)));
+		nullptr, IID_PPV_ARGS(uploadBuffer.GetAddressOf())));
 
 	// 기본 버퍼에 넣을 데이터를 서술하는 구조체
 	D3D12_SUBRESOURCE_DATA subresourceData = {};
 	subresourceData.pData = initData;
 	subresourceData.RowPitch = byteSize;
 	subresourceData.SlicePitch = byteSize;
+
+	/*cmdList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(
+		defaultResource.Get(), D3D12_RESOURCE_STATE_COMMON,
+		D3D12_RESOURCE_STATE_COPY_DEST));*/
 
 	UpdateSubresources(cmdList, defaultResource.Get(), 
 		uploadBuffer.Get(), 0, 0, 1, &subresourceData);
