8c0c223398f0e3248eb5893aa33f756b166cdf51
Tomson0119 <kjs980119@gmail.com>
2021-05-30 16:12:05 +0900

    Getter 추가 및 상수 버퍼의 오프셋을 관리하는 CBIndex 추가

--- MyFramework/MyProject/gameObject.cpp
+++ MyFramework/MyProject/gameObject.cpp
@@ -1,8 +1,10 @@
 #include "../MyCommon/stdafx.h"
 #include "gameObject.h"
 
-GameObject::GameObject()
+GameObject::GameObject(int offset, Mesh* mesh)
 {
+	mCBIndex = (UINT)offset;
+	mMesh = mesh;
 }
 
 GameObject::~GameObject()
@@ -11,42 +13,70 @@ GameObject::~GameObject()
 
 void GameObject::Update()
 {
+	
 }
 
-void GameObject::Draw()
+void GameObject::Draw(ID3D12GraphicsCommandList* cmdList)
 {
+	if (mMesh) {
+		mMesh->Draw(cmdList);
+	}
 }
 
 void GameObject::SetPosition(float x, float y, float z)
 {
+	mWorld._41 = x;
+	mWorld._42 = y;
+	mWorld._43 = z;
 }
 
 void GameObject::SetPosition(XMFLOAT3 pos)
 {
+	SetPosition(pos.x, pos.y, pos.z);
 }
 
 void GameObject::Move(float dx, float dy, float dz)
 {
+	if (dx != 0.0f) MoveStrafe(dx);
+	if (dy != 0.0f) MoveUp(dy);
+	if (dz != 0.0f) MoveForward(dz);
 }
 
 void GameObject::MoveStrafe(float dist)
 {
+	XMFLOAT3 position = GetPosition();
+	XMFLOAT3 right = GetRight();
+	position = Vector3::Add(position, right, dist);
+	SetPosition(position);
 }
 
 void GameObject::MoveUp(float dist)
 {
+	XMFLOAT3 position = GetPosition();
+	XMFLOAT3 up = GetUp();
+	position = Vector3::Add(position, up, dist);
+	SetPosition(position);
 }
 
-void GameObject::MoveForward(float fdist)
+void GameObject::MoveForward(float dist)
 {
+	XMFLOAT3 position = GetPosition();
+	XMFLOAT3 look = GetLook();
+	position = Vector3::Add(position, look, dist);
+	SetPosition(position);
 }
 
 void GameObject::Rotate(float pitch, float yaw, float roll)
 {
+	XMMATRIX rotate = XMMatrixRotationRollPitchYaw(
+		XMConvertToRadians(pitch), XMConvertToRadians(yaw), XMConvertToRadians(roll));
+	mWorld = Matrix4x4::Multiply(rotate, mWorld);
 }
 
-void GameObject::Rotate(XMFLOAT3 axis, float angle)
+void GameObject::Rotate(const XMFLOAT3* axis, float angle)
 {
+	XMMATRIX rotate = XMMatrixRotationAxis(XMLoadFloat3(axis), XMConvertToRadians(angle));
+	mWorld = Matrix4x4::Multiply(rotate, mWorld);
 }
 
 void GameObject::Scale(float xScale, float yScale, float zScale)
@@ -57,14 +87,22 @@ void GameObject::Scale(float scale)
 {
 }
 
-ColoredObject::ColoredObject()
+XMFLOAT3 GameObject::GetPosition()
 {
+	return XMFLOAT3(mWorld._41, mWorld._42, mWorld._43);
 }
 
-ColoredObject::~ColoredObject()
+XMFLOAT3 GameObject::GetLook()
 {
+	return Vector3::Normalize(XMFLOAT3(mWorld._31, mWorld._32, mWorld._33));
 }
 
-void ColoredObject::SetColor(XMFLOAT4 color)
+XMFLOAT3 GameObject::GetUp()
 {
+	return Vector3::Normalize(XMFLOAT3(mWorld._21, mWorld._22, mWorld._23));
+}
+
+XMFLOAT3 GameObject::GetRight()
+{
+	return Vector3::Normalize(XMFLOAT3(mWorld._11, mWorld._12, mWorld._13));
 }
--- MyFramework/MyProject/gameObject.h
+++ MyFramework/MyProject/gameObject.h
@@ -1,17 +1,17 @@
 #pragma once
 
-#include "../MyCommon/mesh.h"
+#include "mesh.h"
 
-class GameObject // RenderItem
+class GameObject
 {
 public:
-	GameObject();
+	GameObject(int offset, Mesh* mesh);
 	GameObject(const GameObject& rhs) = delete;
 	GameObject& operator=(const GameObject& rhs) = delete;
 	virtual ~GameObject();
 
 	virtual void Update();
-	virtual void Draw();
+	virtual void Draw(ID3D12GraphicsCommandList* cmdList);
 
 	void SetPosition(float x, float y, float z);
 	void SetPosition(XMFLOAT3 pos);
@@ -19,26 +19,25 @@ public:
 	void Move(float dx, float dy, float dz);
 	void MoveStrafe(float dist);
 	void MoveUp(float dist);
-	void MoveForward(float fdist);
+	void MoveForward(float dist);
 
 	void Rotate(float pitch, float yaw, float roll);
-	void Rotate(XMFLOAT3 axis, float angle);
+	void Rotate(const XMFLOAT3 *axis, float angle);
 
 	void Scale(float xScale, float yScale, float zScale);
 	void Scale(float scale);
 
+	XMFLOAT3 GetPosition();
+	XMFLOAT3 GetLook();
+	XMFLOAT3 GetUp();
+	XMFLOAT3 GetRight();
+
+	XMFLOAT4X4 GetWorld() const { return mWorld; }
+	UINT CBIndex() const { return mCBIndex; }
+
 protected:
 	XMFLOAT4X4 mWorld = Matrix4x4::Identity4x4();
 	Mesh* mMesh = nullptr;
-};
+	UINT mCBIndex = 0;
 
-class ColoredObject : public GameObject
-{
-public:
-	ColoredObject();
-	ColoredObject(const GameObject& rhs) = delete;
-	ColoredObject& operator=(const ColoredObject& rhs) = delete;
-	virtual ~ColoredObject();
-
-	void SetColor(XMFLOAT4 color);
 };
