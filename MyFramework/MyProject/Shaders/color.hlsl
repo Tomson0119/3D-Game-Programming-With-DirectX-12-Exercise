6f19e2bff6c7f55573db6964ebaaa0766ef79caa
Tomson0119 <kjs980119@gmail.com>
2021-06-02 23:57:41 +0900

    조명을 추가하기 위한 수정, 광원에서 출발하는 벡터는 이론적으로 광원의 방향 반대인데 이상하게 물체의 바닥면을 비추고 있음 그래서 일단 광원의 방향은 그대로 사용하되 나중에 수정할 필요가 있음

--- MyFramework/MyProject/Shaders/color.hlsl
+++ MyFramework/MyProject/Shaders/color.hlsl
@@ -2,23 +2,21 @@
 
 cbuffer CameraCB : register(b0)
 {
-    matrix gView	 : packoffset(c0);
-    matrix gProj	 : packoffset(c4);
-    matrix gViewProj : packoffset(c8);
-    
+    matrix gView	   : packoffset(c0);
+    matrix gProj	   : packoffset(c4);
+    matrix gViewProj   : packoffset(c8);    
     float3 gCameraPos  : packoffset(c12);
 }
 
 cbuffer LightCB : register(b1)
 {
-    float4 gAmbient;
-    Light gLights[NUM_LIGHTS];
+    float4 gAmbient           : packoffset(c0);
+    Light gLights[NUM_LIGHTS] : packoffset(c1);
 }
 
 cbuffer ObjectCB : register(b2)
 {
-    matrix gWorld : packoffset(c0);
-    
+    matrix gWorld : packoffset(c0);    
     Material gMat : packoffset(c4);
 }
 
--- MyFramework/MyProject/Shaders/lighting.hlsl
+++ MyFramework/MyProject/Shaders/lighting.hlsl
@@ -1,4 +1,4 @@
-#define NUM_LIGHTS 1
+#define NUM_LIGHTS 3
 
 struct Light
 {
@@ -21,23 +21,22 @@ float Pow5(float x)
 
 float3 CalcReflectPercent(float3 fresnel, float3 h, float3 l)
 {
-    float3 result = float3(0.0f, 0.0f, 0.0f);
-    float oneMinusCosine = (1 - dot(h, l));
-    
-    return fresnel + (1 - fresnel) * Pow5(oneMinusCosine);
+    float oneMinusCosine = 1.0f - saturate(dot(h, l));
+    return fresnel + (1.0f - fresnel) * Pow5(oneMinusCosine);
 }
 
 float3 PhongModelLighting(float3 lightDiff, float3 lightVec, float3 normal, float3 view, Material mat)
 {
-    const float m = (1.0f - mat.Roughness) * 256.0f;
+    const float shininess = 1.0f - mat.Roughness;
+    const float m = shininess * 256.0f;
     
     float3 halfVec = normalize(view + lightVec);
     
-    float roughness = (m + 8.0f) / 8.0f * pow(max(dot(halfVec, normal), 0.0f), m);
+    float roughness = (m + 8.0f) / 8.0f * pow(saturate(dot(halfVec, normal)), m);
     float3 fresnel = CalcReflectPercent(mat.Fresnel, halfVec, lightVec);    
     float3 specular = fresnel * roughness;
     
-    specular = specular / (specular + 1.0f);
+    //specular = specular / (specular + 1.0f);
     
     float3 matDiffuse = mat.Diffuse.rgb;
     
@@ -46,7 +45,7 @@ float3 PhongModelLighting(float3 lightDiff, float3 lightVec, float3 normal, floa
 
 float3 ComputeDirectLight(Light light, Material mat, float3 normal, float3 view)
 {
-    float3 lightVec = -light.Direction;
+    float3 lightVec = normalize(light.Direction);
     
     float cosValue = max(dot(normal, lightVec), 0.0f);
     float3 lightDiffuse = light.Diffuse * cosValue;
@@ -56,14 +55,15 @@ float3 ComputeDirectLight(Light light, Material mat, float3 normal, float3 view)
 
 float4 ComputeLighting(Light lights[NUM_LIGHTS], Material mat, float3 normal, float3 view)
 {
-    float3 result = float3(0.0f, 0.0f, 0.0f);
+    float3 result = 0.0f;
     
     int i = 0;
+    [unroll]
     for (i = 0; i < NUM_LIGHTS; ++i)
     {
         result += ComputeDirectLight(lights[i], mat, normal, view);
     }
     
-    return float4(result, 1.0f);
+    return float4(result, 0.0f);
 }
 
