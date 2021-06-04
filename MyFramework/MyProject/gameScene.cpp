83824e1c19c740f883a01843e643e7e3e6f5d223
Tomson0119 <kjs980119@gmail.com>
2021-06-04 14:15:35 +0900

    바운딩 박스를 그리는 파이프라인 구축, 마우스 움직임 수정

--- MyFramework/MyProject/gameScene.cpp
+++ MyFramework/MyProject/gameScene.cpp
@@ -4,7 +4,10 @@
 GameScene::GameScene()
 {
 	mCamera = std::make_unique<Camera>();
-	mCamera->SetPosition(0.0f, 2.0f, -10.0f);
+	mCamera->LookAt(
+		XMFLOAT3(0.0f, 5.0f, -10.0f),
+		XMFLOAT3(0.0f, 0.0f, 3.0f),
+		XMFLOAT3(0.0f, 1.0f, 0.0f));
 }
 
 GameScene::~GameScene()
@@ -55,56 +58,55 @@ void GameScene::Update(const GameTimer& timer)
 	mLightCB->CopyData(0, lightCnst);
 
 	for (const auto& obj : mGameObjects)
-	{
-		obj->Update();  // 오브젝트의 상태를 업데이트한다.
-		
 		// 오브젝트로부터 상수들을 받아 업데이트한다.
-		mObjectCB->CopyData(obj->CBIndex(), obj->GetObjectConstants());
-	}
+		obj->Update(mObjectCB.get());
+
+	for (const auto& bb : mBoundBoxes)
+		// 게임 오브젝트의 개수만큼 Offset을 지정한다.
+		bb->Update(mObjectCB.get());  
 }
 
 void GameScene::Draw(ID3D12GraphicsCommandList* cmdList, const GameTimer& timer)
 {
-	cmdList->SetGraphicsRootSignature(mRootSignature.Get());
+	cmdList->SetGraphicsRootSignature(mRootSignature.Get());	
+	cmdList->SetGraphicsRootConstantBufferView(0, mCameraCB->GetGPUVirtualAddress());
+	cmdList->SetGraphicsRootConstantBufferView(1, mLightCB->GetGPUVirtualAddress());
 	
-	auto cmCB = mCameraCB->Resource();	
-	cmdList->SetGraphicsRootConstantBufferView(0, cmCB->GetGPUVirtualAddress());
-
-	auto litCB = mLightCB->Resource();
-	cmdList->SetGraphicsRootConstantBufferView(1, litCB->GetGPUVirtualAddress());
-	
-	auto pso = (mShowWireFrame) ? mPipelines["wiredColor"].get() : mPipelines["defaultColor"].get();
-
-	cmdList->SetPipelineState(pso->GetPSO());
-	pso->Draw(cmdList, mObjectCB->Resource()->GetGPUVirtualAddress(), mObjectCB->GetByteSize());
-}
-
-void GameScene::OnProcessMouseDown(HWND hwnd, WPARAM buttonState, int x, int y)
-{
-	mLastMousePos.x = x;
-	mLastMousePos.y = y;
-
-	SetCapture(hwnd);
-}
-
-void GameScene::OnProcessMouseUp(WPARAM buttonState, int x, int y)
-{
-	ReleaseCapture();
-}
-
-void GameScene::OnProcessMouseMove(WPARAM buttonState, int x, int y)
-{
-	if (buttonState & VK_LBUTTON)
+	if (!mShowWireFrame)
+		mPipelines["defaultColor"]->SetAndDraw(cmdList, mObjectCB.get());
+	else
 	{
-		float delta_x = XMConvertToRadians(0.25f * static_cast<float>(x - mLastMousePos.x));
-		float delta_y = XMConvertToRadians(0.25f * static_cast<float>(y - mLastMousePos.y));
+		mPipelines["wiredColor"]->SetAndDraw(cmdList, mObjectCB.get());
+		mPipelines["boundingBox"]->SetAndDraw(cmdList, mObjectCB.get());
+	}
+}
+
+void GameScene::OnProcessMouseDown(HWND hwnd, WPARAM buttonState)
+{
+	SetCapture(hwnd);
+	GetCursorPos(&mLastMousePos);
+	SetCursor(NULL);
+}
+
+void GameScene::OnProcessMouseUp(WPARAM buttonState)
+{
+	//ReleaseCapture();
+}
+
+void GameScene::OnProcessMouseMove(WPARAM buttonState)
+{
+	if (GetCapture() != nullptr) {
+		POINT currMousePos;
+		GetCursorPos(&currMousePos);
+		SetCursorPos(mLastMousePos.x, mLastMousePos.y);
+
+		float delta_x = XMConvertToRadians(0.25f * static_cast<float>(currMousePos.x - mLastMousePos.x));
+		float delta_y = XMConvertToRadians(0.25f * static_cast<float>(currMousePos.y - mLastMousePos.y));
 
 		// Rotate camera local axis
 		mCamera->RotateY(delta_x);
 		mCamera->Pitch(delta_y);
 	}
-	mLastMousePos.x = x;
-	mLastMousePos.y = y;
 }
 
 void GameScene::OnProcessKeyInput(UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -133,6 +135,15 @@ void GameScene::ProcessInputKeyboard(const GameTimer& timer)
 
 	if (GetAsyncKeyState('D') & 0x8000)
 		mCamera->Move(10.0f * dt, 0.0f, 0.0f);
+
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000)
+		mCamera->Move(XMFLOAT3(0.0f, 1.0f, 0.0f), 10.0f * dt);
+
+	if (GetAsyncKeyState(VK_CONTROL) & 0x8000)
+		mCamera->Move(XMFLOAT3(0.0f, 1.0f, 0.0f), -10.0f * dt);
+
+	if (GetAsyncKeyState(VK_UP) & 0x8000)
+		mPlayer->MoveForward(5.0f * dt);
 }
 
 void GameScene::BuildRootSignature(ID3D12Device* device)
@@ -160,21 +171,65 @@ void GameScene::BuildRootSignature(ID3D12Device* device)
 
 void GameScene::BuildGameObjects(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList)
 {
-	mMeshes["car"] = std::make_unique<Mesh>();
-	mMeshes["car"]->LoadFromBinary(device, cmdList, L"Models\\racing_car.bin");
+	mMeshes["racing_car"] = std::make_unique<CarMesh>(device, cmdList, L"Models\\racing_car.bin");
+	mMeshes["police_car"] = std::make_unique<CarMesh>(device, cmdList, L"Models\\police_car.bin");
+	mMeshes["box1"] = std::make_unique<BoxMesh>(device, cmdList, 2.6f, 0.2f, 400.0f);
+	mMeshes["box2"] = std::make_unique<BoxMesh>(device, cmdList, 0.2f, 0.2f, 400.0f);
 
-	auto carObject = std::make_unique<ColorObject>(0, mMeshes["car"].get());
-	carObject->SetPosition(0.0f, 0.0f, 0.0f);
-	carObject->SetMaterial(XMFLOAT4(0.6f, 0.4f, 0.4f, 1.0f), XMFLOAT3(0.1f, 0.1f, 0.1f), 0.125f);
+	UINT index = 0;
 
-	mPipelines["defaultColor"]->SetObject(carObject.get());
-	mPipelines["wiredColor"]->SetObject(carObject.get());
-	mGameObjects.push_back(std::move(carObject));
+	// 플레이어 객체 생성
+	auto racing_car = std::make_unique<Player>(index++, mMeshes["racing_car"].get());
+	racing_car->SetPosition(0.0f, 0.0f, 0.0f);
+	racing_car->SetMaterial(XMFLOAT4(0.6f, 0.4f, 0.4f, 1.0f), XMFLOAT3(0.0f, 0.0f, 0.0f), 0.125f);
+	racing_car->EnableBoundBoxRender(index++, device, cmdList);  // 바운딩 박스를 렌더링하도록 오브젝트를 생성한다.
+
+	mPlayer = racing_car.get();
+	mPipelines["defaultColor"]->SetObject(racing_car.get());
+	mPipelines["wiredColor"]->SetObject(racing_car.get());
+	mPipelines["boundingBox"]->SetObject(racing_car->GetBoundBoxObject());
+	mBoundBoxes.push_back(racing_car->GetBoundBoxObject());
+	mGameObjects.push_back(std::move(racing_car));
+
+	// 도로를 구분하는 구분선
+	for (int i = 0; i < 6; ++i)
+	{
+		auto line = std::make_unique<GameObject>(index++, mMeshes["box2"].get());
+		line->SetMaterial(XMFLOAT4(1.0f, 0.5f, 0.1f, 1.0f), XMFLOAT3(0.1f, 0.1f, 0.1f), 0.4f);
+		line->SetPosition(-7.0f + 2.8f * i, -0.1f, 190.0f);
+		mPipelines["defaultColor"]->SetObject(line.get());
+		mGameObjects.push_back(std::move(line));
+	}
+
+	// 아스팔트 도로
+	for (int i = 0; i < 5; ++i) {
+		auto road = std::make_unique<GameObject>(index++, mMeshes["box1"].get());
+		road->SetPosition(-5.6f + 2.8f * i, -0.1f, 190.0f);
+		road->SetMaterial(XMFLOAT4(0.35f, 0.35f, 0.35f, 1.0f), XMFLOAT3(0.0f, 0.0f, 0.0f), 0.2f);
+		mPipelines["defaultColor"]->SetObject(road.get());
+		mPipelines["wiredColor"]->SetObject(road.get());
+		mGameObjects.push_back(std::move(road));
+	}
+
+	for (int i = 0; i < 2; ++i)
+	{
+		auto wall = std::make_unique<GameObject>(index++, mMeshes["box1"].get());
+		wall->SetMaterial(XMFLOAT4(1.0f, 0.8f, 0.5f, 1.0f), XMFLOAT3(0.0f, 0.0f, 0.0f), 0.5f);
+		wall->SetPosition(-7.2f + 14.4f * i, 1.1f, 190.0f);
+		wall->EnableBoundBoxRender(index++, device, cmdList);
+		wall->Rotate(0.0f, 0.0f, 90.0f);
+		mWallObjects[i] = wall.get();
+		mPipelines["defaultColor"]->SetObject(wall.get());
+		mPipelines["wiredColor"]->SetObject(wall.get());
+		mPipelines["boundingBox"]->SetObject(wall->GetBoundBoxObject());
+		mBoundBoxes.push_back(wall->GetBoundBoxObject());
+		mGameObjects.push_back(std::move(wall));
+	}	
 }
 
 void GameScene::BuildConstantBuffers(ID3D12Device* device)
 {
-	mObjectCB = std::make_unique<ConstantBuffer<ObjectConstants>>(device, mGameObjects.size());
+	mObjectCB = std::make_unique<ConstantBuffer<ObjectConstants>>(device, mGameObjects.size() + mBoundBoxes.size());
 	mCameraCB = std::make_unique<ConstantBuffer<CameraConstants>>(device, 1);
 	mLightCB = std::make_unique<ConstantBuffer<LightConstants>>(device, 1);
 }
@@ -182,13 +237,18 @@ void GameScene::BuildConstantBuffers(ID3D12Device* device)
 void GameScene::BuildShadersAndPSOs(ID3D12Device* device)
 {
 	// Shader
-	mShaders["color"] = std::make_unique<ColorShader>(L"Shaders\\color.hlsl");
-	
+	mShaders["defaultColor"] = std::make_unique<ColorShader>(L"Shaders\\defaultColor.hlsl");
+	mShaders["onlyColor"] = std::make_unique<ColorShader>(L"Shaders\\onlyColor.hlsl");
+
 	// Pipeline
 	mPipelines["defaultColor"] = std::make_unique<Pipeline>();
-	mPipelines["defaultColor"]->BuildPipeline(device, mRootSignature.Get(), mShaders["color"].get());
+	mPipelines["defaultColor"]->BuildPipeline(device, mRootSignature.Get(), mShaders["defaultColor"].get());
 
 	// Wired Frame Pipeline
 	mPipelines["wiredColor"] = std::make_unique<Pipeline>(true);
-	mPipelines["wiredColor"]->BuildPipeline(device, mRootSignature.Get(), mShaders["color"].get());
+	mPipelines["wiredColor"]->BuildPipeline(device, mRootSignature.Get(), mShaders["defaultColor"].get());
+
+	// BoundingBox Pipeline
+	mPipelines["boundingBox"] = std::make_unique<Pipeline>(true);
+	mPipelines["boundingBox"]->BuildPipeline(device, mRootSignature.Get(), mShaders["onlyColor"].get());
 }
