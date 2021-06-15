214b381a0cb8365385fb357cc6ab16d6bb7d8a67
Tomson0119 <kjs980119@gmail.com>
2021-06-15 19:16:52 +0900

    하이트맵 이미지 클래스 수정

--- MyFramework/DX12FinalTermProject/heightMapImage.cpp
+++ MyFramework/DX12FinalTermProject/heightMapImage.cpp
@@ -5,19 +5,23 @@ HeightMapImage::HeightMapImage(const std::wstring& path,
 	int width, int depth, const XMFLOAT3& scale)
 	: mWidth(width), mDepth(depth), mScale(scale)
 {
-	HANDLE file = CreateFile(path.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL | FILE_ATTRIBUTE_READONLY, NULL);
+	const int imgSize = width * depth;
 
-	BYTE* pixels = new BYTE[mWidth * mDepth];
-	DWORD read;
-	assert(ReadFile(file, pixels, (mWidth * mDepth), &read, NULL));		
+	BYTE* pixels = new BYTE[imgSize];
+	HANDLE file = CreateFile(path.c_str(), GENERIC_READ, 0, NULL,
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_ATTRIBUTE_READONLY, NULL);
+	
+	DWORD read{ };
+	assert(ReadFile(file, pixels, imgSize, &read, NULL) && "Fail to read height map");
 	CloseHandle(file);
 
-	mPixels.resize(mWidth * mDepth);
-	for (int z = 0; z < mDepth; ++z)
+	mPixels.resize(imgSize);
+	for (size_t z = 0; z < mDepth; ++z)
 	{
-		for (int x = 0; x < mWidth; ++x)
+		for (size_t x = 0; x < mWidth; ++x)
+		{
 			mPixels[x + ((mDepth - 1 - z) * mWidth)] = pixels[x + (z * mWidth)];
+		}
 	}
 	if (pixels) delete[] pixels;
 }
@@ -26,82 +30,56 @@ HeightMapImage::~HeightMapImage()
 {
 }
 
-float HeightMapImage::GetHeight(float fx, float fz, bool reverse)
+float HeightMapImage::GetHeight(float fx, float fz) const
 {
-	fx = fx / mScale.x;
-	fz = fz / mScale.z;
-
 	if (fx < 0.0f || fz < 0.0f || fx >= mWidth || fz >= mDepth)
 		return 0.0f;
 
-	int x = (int)fx, z = (int)fz;
-	float xPercent = fx - x, zPercent = fz - z;
-	
-	if (x >= mWidth - 1 || z >= mDepth - 1)
-		return (float)mPixels[x + (z * mWidth)];
+	const size_t x = (size_t)fx;
+	const size_t z = (size_t)fz;
+	const float xPercent = fx - x;
+	const float zPercent = fz - z;
 
 	float bl = (float)mPixels[x + (z * mWidth)];
-	float br = (float)mPixels[x + 1 + (z * mWidth)];
+	float br = (float)mPixels[(x + 1) + (z * mWidth)];
 	float tl = (float)mPixels[x + ((z + 1) * mWidth)];
-	float tr = (float)mPixels[x + 1 + ((z + 1) * mWidth)];
+	float tr = (float)mPixels[(x + 1) + ((z + 1) * mWidth)];
 	
+	const bool reverse = !(z & 1);
+
 	if (reverse)
 	{
-		if (zPercent >= xPercent)
-			br = bl + (tr - tl);
-		else
-			tl = tr + (bl - br);
+		if (zPercent >= xPercent) br = bl + (tr - tl);
+		else tl = tr + (bl - br);
 	}
 	else
 	{
-		if (zPercent < 1.0f - xPercent)
-			tr = tl + (br - bl);
-		else
-			bl = tl + (br - tr);
+		if (zPercent < 1.0f - xPercent)	tr = tl + (br - bl);
+		else bl = tl + (br - tr);
 	}
 
-	float topHeight = tl * (1.0f - xPercent) + tr * xPercent;
-	float bottomHeight = bl * (1.0f - xPercent) + br * xPercent;
-	float height = bottomHeight * (1.0f - zPercent) + topHeight * zPercent;
+	const float topHeight = std::lerp(tl, tr, xPercent);
+	const float bottomHeight = std::lerp(bl, br, xPercent);
+	const float height = std::lerp(bottomHeight, topHeight, zPercent);
 
-	return height * mScale.y * 0.1f;
+	return height;
 }
 
-XMFLOAT3 HeightMapImage::GetNormal(float fx, float fz, bool reverse)
+XMFLOAT3 HeightMapImage::GetNormal(int x, int z) const
 {
-	if (fx < 0.0f || fz < 0.0f || fx >= mWidth || fz >= mDepth)
+	if (x < 0.0f || z < 0.0f || x >= mWidth || z >= mDepth)
 		return XMFLOAT3(0.0f, 1.0f, 0.0f);
 
-	int x = (int)fx, z = (int)fz;
-	float xPercent = fx - x, zPercent = fz - z;
+	const int index = x + (z * mWidth);
+	const int xAdd = (x < (mWidth - 1)) ? 1 : -1;
+	const int zAdd = (z < (mDepth - 1)) ? mWidth : -mWidth;
 
-	if (x >= mWidth - 1 || z >= mDepth - 1)
-		return XMFLOAT3(0.0f, 1.0f, 0.0f);
-
-	float bl = (float)mPixels[x + (z * mWidth)];
-	float br = (float)mPixels[x + 1 + (z * mWidth)];
-	float tl = (float)mPixels[x + ((z + 1) * mWidth)];
-	float tr = (float)mPixels[x + 1 + ((z + 1) * mWidth)];
-
-	float y1 = 0.0f, y2 = 0.0f, y3 = 0.0f;
-
-	if (reverse)
-	{
-		if (zPercent >= xPercent)
-			y1 = tl, y2 = bl, y3 = tr;
-		else
-			y1 = br, y2 = bl, y3 = tr;
-	}
-	else
-	{
-		if (zPercent < 1.0f - xPercent)
-			y1 = bl, y2 = tl, y3 = br;
-		else
-			y1 = tr, y2 = br, y3 = tl;
-	}
+	const float y1 = (float)mPixels[index] * mScale.y;
+	const float y2 = (float)mPixels[index + xAdd] * mScale.y;
+	const float y3 = (float)mPixels[index + zAdd] * mScale.y;
 
 	XMFLOAT3 edge1 = XMFLOAT3(0.0f, y3 - y1, mScale.z);
 	XMFLOAT3 edge2 = XMFLOAT3(mScale.x, y2 - y1, 0.0f);
 
-	return Vector3::Cross(edge1, edge2);
+	return Vector3::Normalize(Vector3::Cross(edge1, edge2));
 }
--- MyFramework/DX12FinalTermProject/heightMapImage.h
+++ MyFramework/DX12FinalTermProject/heightMapImage.h
@@ -10,15 +10,15 @@ public:
 		const XMFLOAT3& scale);
 	virtual ~HeightMapImage();
 
-	float GetHeight(float fx, float fz, bool reverse);
-	XMFLOAT3 GetNormal(float fx, float fz, bool reverse);
+	float GetHeight(float fx, float fz) const;
+	XMFLOAT3 GetNormal(int x, int z) const;
 
 	std::vector<BYTE> GetPixels() const { return mPixels; }
+	float GetPixelValue(int index) const { return mPixels[index]; }
 
 	XMFLOAT3 GetScale() const { return mScale; }
 	int GetWidth() const { return mWidth; }
 	int GetDepth() const { return mDepth; }
-
 
 private:
 	std::vector<BYTE> mPixels;
