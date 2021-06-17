0796584ba1481f0576c36b97fe6a1b0eab222da5
Tomson0119 <kjs980119@gmail.com>
2021-06-17 18:52:09 +0900

    콘솔 창을 통한 디버그를 하기 위함

--- MyFramework/DX12FinalTermProject/main.cpp
+++ MyFramework/DX12FinalTermProject/main.cpp
@@ -3,6 +3,20 @@
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int mCmdShow)
 {
+#if defined(DEBUG) || defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	if (AllocConsole() == TRUE)
+	{
+		FILE* file;
+		freopen_s(&file, "CONOUT$", "rb", stdin);
+		freopen_s(&file, "CONOUT$", "wb", stdout);
+		freopen_s(&file, "CONOUT$", "wb", stderr);
+		std::ios::sync_with_stdio();
+	}
+	
+#endif
+	std::cout << "Hello" << std::endl;
+
 	try
 	{
 		GameFramework app(1000, 800);
