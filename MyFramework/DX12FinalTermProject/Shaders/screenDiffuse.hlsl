d6caecf264234660f4db39427e8d092a3e073a16
Tomson0119 <kjs980119@gmail.com>
2021-06-18 17:54:53 +0900

    screenDiffuse 파일 : 스크린 좌표를 다루는 셰이더

--- MyFramework/DX12FinalTermProject/Shaders/screenDiffuse.hlsl
+++ MyFramework/DX12FinalTermProject/Shaders/screenDiffuse.hlsl
@@ -2,10 +2,10 @@
 
 cbuffer CameraCB : register(b0)
 {
-    matrix gView	    : packoffset(c0);
-    matrix gProj	    : packoffset(c4);
-    matrix gViewProj    : packoffset(c8); 
-    float3 gCameraPos   : packoffset(c12);
+    matrix gView      : packoffset(c0);
+    matrix gProj      : packoffset(c4);
+    matrix gViewProj  : packoffset(c8);
+    float3 gCameraPos : packoffset(c12);
 }
 
 cbuffer LightCB : register(b1)
@@ -16,32 +16,34 @@ cbuffer LightCB : register(b1)
 
 cbuffer ObjectCB : register(b2)
 {
-    matrix gWorld : packoffset(c0);    
+    matrix gWorld : packoffset(c0);
     Material gMat : packoffset(c4);
 }
 
 struct VertexIn
 {
-	float3 PosL		: POSITION;
-    float4 Color    : COLOR;
+    float3 PosL  : POSITION;
+    float4 Color : COLOR;
 };
 
 struct VertexOut
 {
-	float4 PosH     : SV_POSITION;
-    float3 PosW     : POSITION;
-    float4 Color    : COLOR;
+    float4 PosH  : SV_POSITION;
+    float3 PosW  : POSITION;
+    float4 Color : COLOR;
 };
 
 VertexOut VS(VertexIn vin)
 {
-	VertexOut vout;
+    VertexOut vout;
 	
     vout.PosW = mul(float4(vin.PosL, 1.0f), gWorld).xyz;
     vout.PosH = mul(float4(vout.PosW, 1.0f), gViewProj);
+    vout.PosH.z = 0.0f;
+    
     vout.Color = vin.Color;
     
-	return vout;
+    return vout;
 }
 
 float4 PS(VertexOut pin) : SV_Target
