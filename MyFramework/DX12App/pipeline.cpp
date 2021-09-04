fb64721c107717fefc2e7ae1e528a065f387290d
Tomson0119 <kjs980119@gmail.com>
2021-09-04 22:52:31 +0900

    서술자들을 반환하는 정적 함수 추가

--- MyFramework/DX12App/pipeline.cpp
+++ MyFramework/DX12App/pipeline.cpp
@@ -72,3 +72,60 @@ void Pipeline::SetAndDraw(ID3D12GraphicsCommandList* cmdList, ConstantBuffer<Obj
 		item->Draw(cmdList);
 	}
 }
+
+D3D12_RASTERIZER_DESC Pipeline::DefaultRasterizerDesc()
+{
+	D3D12_RASTERIZER_DESC rasterizerDesc{};
+	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
+	rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
+	rasterizerDesc.FrontCounterClockwise = FALSE;
+	rasterizerDesc.DepthBias = 0;
+	rasterizerDesc.DepthBiasClamp = 0.0f;
+	rasterizerDesc.SlopeScaledDepthBias = 0.0f;
+	rasterizerDesc.DepthClipEnable = TRUE;
+	rasterizerDesc.MultisampleEnable = FALSE;
+	rasterizerDesc.AntialiasedLineEnable = FALSE;
+	rasterizerDesc.ForcedSampleCount = 0;
+	rasterizerDesc.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
+	return rasterizerDesc;
+}
+
+D3D12_DEPTH_STENCIL_DESC Pipeline::DefaultDepthStencilDesc()
+{
+	D3D12_DEPTH_STENCIL_DESC depthStencilDesc{};
+	depthStencilDesc.DepthEnable = TRUE;
+	depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+	depthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS;
+	depthStencilDesc.StencilEnable = FALSE;
+	depthStencilDesc.StencilReadMask = D3D12_DEFAULT_STENCIL_READ_MASK;
+	depthStencilDesc.StencilWriteMask = D3D12_DEFAULT_STENCIL_WRITE_MASK;
+	
+	D3D12_DEPTH_STENCILOP_DESC faceDesc{};
+	faceDesc.StencilFunc = D3D12_COMPARISON_FUNC_ALWAYS;
+	faceDesc.StencilDepthFailOp = D3D12_STENCIL_OP_KEEP;
+	faceDesc.StencilPassOp = D3D12_STENCIL_OP_KEEP;
+	faceDesc.StencilFailOp = D3D12_STENCIL_OP_KEEP;
+
+	depthStencilDesc.FrontFace = faceDesc;
+	depthStencilDesc.BackFace = faceDesc;
+
+	return depthStencilDesc;
+}
+
+D3D12_BLEND_DESC Pipeline::DefaultBlendDesc()
+{
+	D3D12_BLEND_DESC blendDesc{};
+	blendDesc.AlphaToCoverageEnable = FALSE;
+	blendDesc.IndependentBlendEnable = FALSE;
+	blendDesc.RenderTarget[0].BlendEnable = FALSE;
+	blendDesc.RenderTarget[0].LogicOpEnable = FALSE;
+	blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_ONE;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ZERO;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
+	blendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+	blendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].LogicOp = D3D12_LOGIC_OP_NOOP;
+	blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+	return blendDesc;
+}
