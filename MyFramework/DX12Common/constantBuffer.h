2211f26f584d763d36247d4037bc837e5024b96b
Tomson0119 <kjs980119@gmail.com>
2021-09-04 22:52:10 +0900

    extension으로 대체

--- MyFramework/DX12Common/constantBuffer.h
+++ MyFramework/DX12Common/constantBuffer.h
@@ -12,9 +12,9 @@ public:
 		mByteSize = (sizeof(Cnst) + 255) & ~255;
 
 		ThrowIfFailed(device->CreateCommittedResource(
-			&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+			&Extension::HeapProperties(D3D12_HEAP_TYPE_UPLOAD),
 			D3D12_HEAP_FLAG_NONE,
-			&CD3DX12_RESOURCE_DESC::Buffer(mByteSize * count),
+			&Extension::BufferResourceDesc(mByteSize * count),
 			D3D12_RESOURCE_STATE_GENERIC_READ,
 			nullptr, IID_PPV_ARGS(&mUploadBuffer)));
 
