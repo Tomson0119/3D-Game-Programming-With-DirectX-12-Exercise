e940d7041ec67e2292ece9a4cd465287299916e0
Tomson0119 <kjs980119@gmail.com>
2021-09-05 22:46:09 +0900

    해상도 설정

--- MyFramework/DX12App/main.cpp
+++ MyFramework/DX12App/main.cpp
@@ -1,10 +1,10 @@
 #include "stdafx.h"
-#include "framework.h"
+#include "gameFramework.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int mCmdShow)
 {
 #if defined(DEBUG) || defined(_DEBUG)
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	/*_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	if (AllocConsole() == TRUE)
 	{
 		FILE* file;
@@ -12,13 +12,15 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int mCmdShow)
 		freopen_s(&file, "CONOUT$", "wb", stdout);
 		freopen_s(&file, "CONOUT$", "wb", stderr);
 		std::ios::sync_with_stdio();
-	}
+	}*/
 	
 #endif
 
 	try
 	{
-		GameFramework app(1000, 800);
+		GameFramework app;
+		app.SetResolution(1000, 800);
+
 		if (!app.InitFramework())
 			return 0;		
 		app.Run();
