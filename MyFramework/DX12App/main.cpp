1a2c6be7b76325cee05e3bd4c523bdb99efa2fb9
Tomson0119 <kjs980119@gmail.com>
2021-09-06 19:11:44 +0900

    디버그 on

--- MyFramework/DX12App/main.cpp
+++ MyFramework/DX12App/main.cpp
@@ -4,7 +4,7 @@
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int mCmdShow)
 {
 #if defined(DEBUG) || defined(_DEBUG)
-	/*_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	if (AllocConsole() == TRUE)
 	{
 		FILE* file;
@@ -12,7 +12,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int mCmdShow)
 		freopen_s(&file, "CONOUT$", "wb", stdout);
 		freopen_s(&file, "CONOUT$", "wb", stderr);
 		std::ios::sync_with_stdio();
-	}*/
+	}
 	
 #endif
 
