be1bd2e60b7d66e3bf3e023e76dacd5abcf83ec3
Tomson0119 <kjs980119@gmail.com>
2021-09-07 15:26:29 +0900

    ...

--- MyFramework/DX12App/gameScene.h
+++ MyFramework/DX12App/gameScene.h
@@ -21,7 +21,6 @@ public:
 
 	void BuildObjects(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList);
 
-	void Resize(float aspect);
 	void UpdateConstants(Camera* camera);
 	void Update(const GameTimer& timer);
 	void Draw(ID3D12GraphicsCommandList* cmdList);
@@ -29,7 +28,7 @@ public:
 	void OnProcessMouseDown(WPARAM buttonState, int x, int y) {}
 	void OnProcessMouseUp(WPARAM buttonState, int x, int y) {}
 	void OnProcessMouseMove(WPARAM buttonState) {}
-	void OnProcessKeyInput(UINT uMsg, WPARAM wParam, LPARAM lParam) {}
+	void OnProcessKeyInput(UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 	void OnPreciseKeyInput(const GameTimer& timer) {}
 
@@ -40,12 +39,12 @@ private:
 	void BuildGameObjects(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList);
 	void BuildConstantBuffers(ID3D12Device* device);
 	void BuildShadersAndPSOs(ID3D12Device* device);
-	void BuildTextures(ID3D12Device* device);
+	void BuildDescriptorHeap(ID3D12Device* device);
 
 private:
 	XMFLOAT4 mFrameColor = (XMFLOAT4)Colors::LightSkyBlue;
 
-	std::unique_ptr<ConstantBuffer<ObjectConstants>> mObjectCB;
+	//std::unique_ptr<ConstantBuffer<ObjectConstants>> mObjectCB;
 	std::unique_ptr<ConstantBuffer<CameraConstants>> mCameraCB;
 	std::unique_ptr<ConstantBuffer<LightConstants>> mLightCB;
 
@@ -55,4 +54,6 @@ private:
 	std::unordered_map<std::string, std::unique_ptr<Texture>> mTextures;
 
 	UINT mGameObjectCount = 0;
+	
+	bool mShowWired = false;
 };
