d7fe03fa41c7bf7e8188007f2af1ca90f10a7217
Tomson0119 <kjs980119@gmail.com>
2021-09-07 15:26:51 +0900

    descriptor heap desc 생성 함수

--- MyFramework/DX12App/d3dExtension.h
+++ MyFramework/DX12App/d3dExtension.h
@@ -124,4 +124,15 @@ namespace Extension
 		rootSigDesc.Flags = flags;
 		return rootSigDesc;
 	}
+
+	inline D3D12_DESCRIPTOR_HEAP_DESC DescriptorHeapDesc(
+		UINT numDescriptors)
+	{
+		D3D12_DESCRIPTOR_HEAP_DESC descriptorHeapDesc{};
+		descriptorHeapDesc.NumDescriptors = numDescriptors;
+		descriptorHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+		descriptorHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+		descriptorHeapDesc.NodeMask = 0;
+		return descriptorHeapDesc;
+	}
 }
