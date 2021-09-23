d72dc0128b832ad57e01534b259c90224d545863
Tomson0119 <kjs980119@gmail.com>
2021-09-23 21:13:37 +0900

    refactor - vertex 이동
    vertex들을 vertex.h 파일로 이동

--- MyFramework/DX12App/mesh.h
+++ MyFramework/DX12App/mesh.h
@@ -1,32 +1,7 @@
 #pragma once
 
 #include "heightMapImage.h"
-
-struct Vertex
-{
-	Vertex() = default;
-	Vertex(float x,  float y,  float z,
-		   float nx, float ny, float nz,
-		   float u,  float v)
-		: Position(x, y, z), Normal(nx, ny, nz), TexCoord(u, v) { }
-
-	Vertex(const XMFLOAT3& pos,
-		   const XMFLOAT3& normal,
-		   const XMFLOAT2& texC)
-		: Position(pos), Normal(normal), TexCoord(texC) { }
-
-	XMFLOAT3 Position;
-	XMFLOAT3 Normal;
-	XMFLOAT2 TexCoord;
-};
-
-struct DiffuseTexVertex
-{
-	XMFLOAT3 Position;
-	XMFLOAT4 Color;
-	XMFLOAT2 TexCoord;
-};
-
+#include "vertex.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 //
