60039a0155fe03c05689e62a4411eae183fc3c2e
Tomson0119 <kjs980119@gmail.com>
2021-10-18 22:25:14 +0900

    refactor - 노멀 맵 적용
    넘겨준 정점의 탄젠트 좌표를 이용해
    노멀맵에서 읽은 노멀 값을 변환하여 빛을 계산한다.

--- MyFramework/DX12App/Shaders/defaultLit.hlsl
+++ MyFramework/DX12App/Shaders/defaultLit.hlsl
@@ -1,6 +1,8 @@
 #include "lighting.hlsl"
 
-Texture2D gTexture : register(t0);
+Texture2D gDiffuseMap : register(t0);
+Texture2D gNormalMap  : register(t1);
+
 SamplerState gSampler : register(s0);
 
 cbuffer CameraCB : register(b0)
@@ -28,6 +30,7 @@ struct VertexIn
 {
 	float3 PosL		: POSITION;
     float3 NormalL	: NORMAL;
+    float3 TangentL : TANGENT;
     float2 TexCoord : TEXCOORD;
 };
 
@@ -36,6 +39,7 @@ struct VertexOut
 	float4 PosH     : SV_POSITION;
     float3 PosW     : POSITION;
     float3 NormalW  : NORMAL;
+    float3 TangentW : TANGENT;
     float2 TexCoord : TEXCOORD;
 };
 
@@ -46,7 +50,10 @@ VertexOut VS(VertexIn vin)
     vout.PosW = mul(float4(vin.PosL, 1.0f), gWorld).xyz;
     vout.PosH = mul(float4(vout.PosW, 1.0f), gViewProj);
     
-    vout.NormalW = mul(vin.NormalL, (float3x3) gWorld);
+    float4x4 tWorld = transpose(gWorld);
+    vout.NormalW = mul((float3x3)tWorld, vin.NormalL);
+    vout.TangentW = mul((float3x3)tWorld, vin.TangentL);
+    
     vout.TexCoord = vin.TexCoord;
 	
 	return vout;
@@ -54,15 +61,22 @@ VertexOut VS(VertexIn vin)
 
 float4 PS(VertexOut pin) : SV_Target
 {  
-    float4 diffuse = gTexture.Sample(gSampler, pin.TexCoord) * gMat.Diffuse;
+    float4 diffuse = gDiffuseMap.Sample(gSampler, pin.TexCoord) * gMat.Diffuse;
+    float4 normalColor = gNormalMap.Sample(gSampler, pin.TexCoord);
     
-    pin.NormalW = normalize(pin.NormalW);
+    float3 normalT = 2.0f * normalColor.rgb - 1.0f;
+    
+    float3 N = normalize(pin.NormalW);
+    float3 T = normalize(pin.TangentW - dot(pin.TangentW, N) * N);
+    float3 B = cross(N, T);
+    
+    float3 normalW = mul(normalT, float3x3(T, B, N));
     
     float3 view = normalize(gCameraPos - pin.PosW);
     float4 ambient = gAmbient * diffuse;
     
     Material mat = { diffuse, gMat.Fresnel, gMat.Roughness };
-    float4 directLight = ComputeLighting(gLights, mat, pin.NormalW, view);
+    float4 directLight = ComputeLighting(gLights, mat, normalW, view);
     
     float4 result = ambient + directLight;
     result.a = gMat.Diffuse.a;
