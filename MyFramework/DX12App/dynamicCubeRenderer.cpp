2f5ac61ef98847c1ebc1d1101b950e04d68c59e4
Tomson0119 <kjs980119@gmail.com>
2021-10-24 16:35:34 +0900

    add - 동적 큐브맵에 사용되는 파이프라인
    파이프라인을 상속하는 동적 큐브맵 렌더러를 생성하였다.
    이 클래스에서는 오브젝트를 받는 즉시, 큐브맵 텍스쳐 리소스를 생성하고
    새로운 렌더타겟과 깊이스텐실 서술자 힙을 생성한다. 그리고 모든 오브젝트마다 cpu 핸들 값을 넘겨주어
    오브젝트가 cpu 핸들을 초기화할 수 있도록 하였다.

---
+++ MyFramework/DX12App/dynamicCubeRenderer.cpp
@@ -0,0 +1,76 @@
+#include "stdafx.h"
+#include "dynamicCubeRenderer.h"
+
+#include "gameObject.h"
+#include "texture.h"
+#include "gameScene.h"
+
+DynamicCubeRenderer::DynamicCubeRenderer()
+{
+}
+
+DynamicCubeRenderer::~DynamicCubeRenderer()
+{
+}
+
+void DynamicCubeRenderer::AppendObject(ID3D12Device* device, const std::shared_ptr<DynamicCubeMapObject> obj)
+{
+	mRenderObjects.push_back(obj);
+	CreateTexture(device, obj);
+}
+
+void DynamicCubeRenderer::PreDraw(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, GameScene* scene)
+{
+	for (int i = 0; i < mRenderObjects.size(); i++)
+	{
+		mRenderObjects[i]->PreDraw(cmdList, mTextures[i]->GetResource(), scene);
+	}
+}
+
+void DynamicCubeRenderer::CreateTexture(ID3D12Device* device, const std::shared_ptr<DynamicCubeMapObject> obj)
+{
+	std::unique_ptr<Texture> textureCube = std::make_unique<Texture>();
+	textureCube->SetDimension(D3D12_SRV_DIMENSION_TEXTURECUBE);
+
+	D3D12_CLEAR_VALUE clearValue = { DXGI_FORMAT_R8G8B8A8_UNORM, {0.0f,0.0f,0.0f,1.0f} };
+
+	UINT size = (UINT)obj->GetCubeMapSize();
+
+	textureCube->CreateTexture(
+		device, size, size, RtvCounts, 1,
+		DXGI_FORMAT_R8G8B8A8_UNORM,
+		D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET,
+		D3D12_RESOURCE_STATE_GENERIC_READ, &clearValue);
+
+	mTextures.push_back(std::move(textureCube));
+}
+
+void DynamicCubeRenderer::BuildDescriptorHeap(ID3D12Device* device, UINT cbvIndex, UINT srvIndex)
+{
+	Pipeline::BuildDescriptorHeap(device, cbvIndex, srvIndex);
+
+	ThrowIfFailed(device->CreateDescriptorHeap(
+		&Extension::DescriptorHeapDesc(
+			(UINT)mRenderObjects.size(),
+			D3D12_DESCRIPTOR_HEAP_TYPE_DSV,
+			D3D12_DESCRIPTOR_HEAP_FLAG_NONE),
+		IID_PPV_ARGS(&mDsvDescriptorHeap)));
+
+	ThrowIfFailed(device->CreateDescriptorHeap(
+		&Extension::DescriptorHeapDesc(
+			(UINT)mRenderObjects.size() * RtvCounts,
+			D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
+			D3D12_DESCRIPTOR_HEAP_FLAG_NONE),
+		IID_PPV_ARGS(&mRtvDescriptorHeap)));
+
+	for (int i = 0; i < mRenderObjects.size(); i++)
+	{
+		auto rtvHandle = mRtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+		auto dsvHandle = mDsvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+
+		rtvHandle.ptr += i * gRtvDescriptorSize * 6;
+		dsvHandle.ptr += i * gDsvDescriptorSize;
+
+		mRenderObjects[i]->BuildDsvRtvView(device, mTextures[i]->GetResource(), rtvHandle, dsvHandle);
+	}
+}
---
+++ MyFramework/DX12App/dynamicCubeRenderer.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "pipeline.h"
+
+class DynamicCubeMapObject;
+class Texture;
+class GameScene;
+
+class DynamicCubeRenderer : public Pipeline
+{
+public :
+	DynamicCubeRenderer();
+	~DynamicCubeRenderer();
+
+	virtual void BuildDescriptorHeap(ID3D12Device* device, UINT cbvIndex, UINT srvIndex);
+
+	void AppendObject(ID3D12Device* device, const std::shared_ptr<DynamicCubeMapObject> obj);
+	void PreDraw(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, GameScene* scene);
+
+private:
+	void CreateTexture(ID3D12Device* device, const std::shared_ptr<DynamicCubeMapObject> obj);
+
+private:
+	static const int RtvCounts = 6;
+
+	ComPtr<ID3D12DescriptorHeap> mRtvDescriptorHeap;
+	ComPtr<ID3D12DescriptorHeap> mDsvDescriptorHeap;
+};
