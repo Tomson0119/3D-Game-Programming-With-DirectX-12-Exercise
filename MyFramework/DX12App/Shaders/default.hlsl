eebd259c82c4739895e55a1937bbb7fdcb7b87a1
Tomson0119 <kjs980119@gmail.com>
2021-10-30 21:38:08 +0900

    misc - 파일 이름 변경, 파일 추가 삭제

---
+++ MyFramework/DX12App/Shaders/default.hlsl
@@ -0,0 +1,52 @@
+#include "common.hlsl"
+
+Texture2D gDiffuseMap : register(t0);
+
+struct VertexIn
+{
+	float3 PosL		: POSITION;
+    float3 NormalL	: NORMAL;
+    float3 TangentL : TANGENT;
+    float2 TexCoord : TEXCOORD;
+};
+
+struct VertexOut
+{
+	float4 PosH     : SV_POSITION;
+    float3 PosW     : POSITION;
+    float3 NormalW  : NORMAL;
+    float3 TangentW : TANGENT;
+    float2 TexCoord : TEXCOORD;
+};
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout;
+	
+    vout.PosW = mul(float4(vin.PosL, 1.0f), gWorld).xyz;
+    vout.PosH = mul(float4(vout.PosW, 1.0f), gViewProj);
+    
+    float4x4 tWorld = transpose(gWorld);
+    vout.NormalW = mul((float3x3)tWorld, vin.NormalL);
+    vout.TangentW = mul((float3x3)tWorld, vin.TangentL);
+    
+    vout.TexCoord = vin.TexCoord;
+	
+	return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{  
+    float4 diffuse = gDiffuseMap.Sample(gAnisotropicWrap, pin.TexCoord) * gMat.Diffuse;
+    
+    float3 view = normalize(gCameraPos - pin.PosW);
+    float4 ambient = gAmbient * diffuse;
+    
+    Material mat = { diffuse, gMat.Fresnel, gMat.Roughness };
+    float4 directLight = ComputeLighting(gLights, mat, pin.NormalW, view);
+    
+    float4 result = ambient + directLight;
+    result.a = gMat.Diffuse.a;
+    
+    return result;
+}
